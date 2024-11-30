104383f660715934e695b2145be3ae9980994733
CarlMatthewVerano <carlmatthewverano@gmail.com>
2024-11-30 22:30:39 +0800

    feat: finished

---
+++ app/src/main/java/com/mobdeve/tighee/contactsapp/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.mobdeve.tighee.contactsapp
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.mobdeve.tighee.contactsapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val TAG = "MAIN_ACTIVITY"
+        private const val CONTACTS_STRING_HOLDER_KEY = "CONTACTS_STRING_HOLDER_KEY"
+    }
+
+    private lateinit var viewBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        viewBinding.addBtn.setOnClickListener(View.OnClickListener {
+            val name = viewBinding.nameEtv.text.toString()
+            val number = viewBinding.numberEtv.text.toString()
+
+            if (name != "" || number != "") {
+                Log.d(TAG, "onClick: added properly")
+                val temp = "${viewBinding.contactsStringHolderTv.text}$name : $number\n"
+                viewBinding.contactsStringHolderTv.setText(temp)
+
+                viewBinding.nameEtv.setText("")
+                viewBinding.numberEtv.setText("")
+            } else {
+                Log.d(TAG, "onClick: not added properly")
+            }
+        })
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val sp: SharedPreferences = getPreferences(Context.MODE_PRIVATE)
+        var displayString = sp.getString(CONTACTS_STRING_HOLDER_KEY, "")
+
+        if (!displayString.equals("")) {
+            displayString = displayString!!.trim() + '\n'
+        }
+
+        viewBinding.contactsStringHolderTv.text = displayString
+        Log.d(TAG, "read data:\n|" + viewBinding.contactsStringHolderTv.text.toString() + '|')
+
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        val editor: SharedPreferences.Editor = getPreferences(Context.MODE_PRIVATE).edit()  // get the SharedPreferences associated with this activity
+        editor.putString(CONTACTS_STRING_HOLDER_KEY, viewBinding.contactsStringHolderTv.text.toString())
+        editor.apply()
+        Log.d(TAG, "onPause: saved data")
+    }
+}
